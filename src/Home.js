7e4d01e12744e5a61cc3f0feee0f409c7ecba7c0
Nagesh2003 <codeinovator@gmail.com>
2025-04-02 16:54:58 +0530

    update

---
+++ src/Home.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import Header from './Header';
+import About from './About';
+import Services from './Services';
+import Features from './Features';
+import Contact from './Contact';
+import Footer from './Footer';
+
+const Home = () => {
+  return (
+    <div style={styles.container}>
+      <Header />
+      <About />
+      <Services />
+      <Features />
+     
+      <Contact />
+      <Footer />
+    </div>
+  );
+};
+
+const styles = {
+  container: {
+    overflowY: 'scroll',
+    height: '100vh',
+    display: 'flex',
+    flexDirection: 'column',
+  },
+};
+
+export default Home;
